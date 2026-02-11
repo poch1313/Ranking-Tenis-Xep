bd6c8f00b4c95df7c4a2b0829a166b7c7a23e067
poch1313 <102809149+poch1313@users.noreply.github.com>
2026-02-11 10:19:35 -0300

    Change match_time input to text input for HH:MM format

--- codigo_general.py
+++ codigo_general.py
@@ -213,7 +213,7 @@ elif menu == "Invitación Abierta":
     with st.form("invitation_form"):
         created_by = st.selectbox("Soy:", list(players_emails.keys()))
         match_date = st.date_input("Fecha")
-        match_time = st.time_input("Hora",value=datetime.now().time(),step=60)
+        match_time_str = st.text_input("Hora (HH:MM)",placeholder="Ejemplo: 19:30").strip()
         location = st.text_input("Lugar")
         submit = st.form_submit_button("Enviar Invitación")
 
